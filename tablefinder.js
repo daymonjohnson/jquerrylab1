b28b725b58e69687e5d5ff2aa1d2232c99f7c60e
DJ <daymonjohnson75@yahoo.com>
2018-05-08 22:09:16 -0400

    initial commit

--- tablefinder.js
+++ tablefinder.js
@@ -1,33 +1,39 @@
 $(".form").hide();
+$(".details").hide();
 
 $(".available").on("click", () => {
-    $(".form").hide().delay(100).fadeIn(100);
+    $(".form").hide().delay(100).fadeIn(3000);
 })
 $("#1").click(function(){
     $("<span> 1</span>").appendTo("h4");
     $(".savebutton").click(function(){
-        $(".form").hide();
+        $("#1").css("background-color", "blue");
+        $(".form").fadeOut(3000);
         $("span").detach();
         $('#1').removeClass('available').addClass('reserved');
+        $("<div class = 'tableOne'></div>").appendTo("#1");
+        let oneName = $("#name").val();
+        $(".tableOne").append("Name: " + oneName + "<br>");
+        let oneNumber = $("#number").val();
+        $(".tableOne").append("Number: " + oneNumber + "<br>");
+        let oneSeats = $("#seats").val();
+        $(".tableOne").append("Seats: " + oneSeats + "<br>");
     });
 });
 $("#2").click(function(){
     $("<span> 2</span>").appendTo("h4");
     $(".savebutton").click(function(){
         $("#2").css("background-color", "blue");
-        $("#2").css("color", "white");
-        $(".form").hide();
+        $(".form").fadeOut(3000);
         $("span").detach();
         $('#2').removeClass('available').addClass('reserved');
-
     });
 });
 $("#3").click(function(){
     $("<span> 3</span>").appendTo("h4");
     $(".savebutton").click(function(){
         $("#3").css("background-color", "blue");
-        $("#3").css("color", "white");
-        $(".form").hide();
+        $(".form").fadeOut(3000);
         $("span").detach();
         $('#3').removeClass('available').addClass('reserved');
 
@@ -37,8 +43,7 @@ $("#4").click(function(){
     $("<span> 4</span>").appendTo("h4");
     $(".savebutton").click(function(){
         $("#4").css("background-color", "blue");
-        $("#4").css("color", "white");
-        $(".form").hide();
+        $(".form").fadeOut(3000);
         $("span").detach();
         $('#4').removeClass('available').addClass('reserved');
 
@@ -48,8 +53,7 @@ $("#5").click(function(){
     $("<span> 5</span>").appendTo("h4");
     $(".savebutton").click(function(){
         $("#5").css("background-color", "blue");
-        $("#5").css("color", "white");
-        $(".form").hide();
+        $(".form").fadeOut(3000);
         $("span").detach();
         $('#5').removeClass('available').addClass('reserved');
 
@@ -59,19 +63,17 @@ $("#6").click(function(){
     $("<span> 6</span>").appendTo("h4");
     $(".savebutton").click(function(){
         $("#6").css("background-color", "blue");
-        $("#6").css("color", "white");
-        $(".form").hide();
+        $(".form").fadeOut(3000);
         $("span").detach();
         $('#6').removeClass('available').addClass('reserved');
 
     });
 });
 $("#7").click(function(){
-    $("<span> 1</span>").appendTo("h4");
+    $("<span> 7</span>").appendTo("h4");
     $(".savebutton").click(function(){
         $("#7").css("background-color", "blue");
-        $("#7").css("color", "white");
-        $(".form").hide();
+        $(".form").fadeOut(3000);
         $("span").detach();
         $('#7').removeClass('available').addClass('reserved');
     });
@@ -80,8 +82,7 @@ $("#8").click(function(){
     $("<span> 8</span>").appendTo("h4");
     $(".savebutton").click(function(){
         $("#8").css("background-color", "blue");
-        $("#8").css("color", "white");
-        $(".form").hide();
+        $(".form").fadeOut(3000);
         $("span").detach();
         $('#8').removeClass('available').addClass('reserved');
     });
@@ -90,10 +91,19 @@ $("#9").click(function(){
     $("<span> 9</span>").appendTo("h4");
     $(".savebutton").click(function(){
         $("#9").css("background-color", "blue");
-        $("#9").css("color", "white");
-        $(".form").hide();
+        $(".form").fadeOut(3000);
         $("span").detach();
-        $('#1').removeClass('available').addClass('reserved');
+        $('#9').removeClass('available').addClass('reserved');
 
     });
 });
+$(".cancelbutton").click(function(){
+    $(".form").fadeOut(1000);
+});
+
+
+$( ".form" ).submit(function() {
+    return this.name+this.seats;
+  });
+
+  
